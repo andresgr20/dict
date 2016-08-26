14ac02778e805a1b3aa6e86d349dc8f7bc9a6076
Andres Garcia Rdz <andresgr2094@hotmail.com>
2016-08-26 16:13:49 -0400

    added the emojis

--- Dict/ViewController.swift
+++ Dict/ViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate{
 
     @IBOutlet weak var backgroundTable: UITableView!
+    var emojis = ["😳","💦","🙊","👯","👌🏿","💥","🙈","😘"]
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,12 +21,12 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return emojis.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell()
-        cell.textLabel?.text = "😳"
+        cell.textLabel?.text = emojis[indexPath.row]
         return cell
         
     }
